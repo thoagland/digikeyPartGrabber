207addc6e0192f4ed0a3648da0a87195399d2377
Tim Hoagland <tim.hoagland@lightstormresearch.com>
2019-01-05 14:30:03 -0500

    Updated Readme

---
+++ digikeyPartGrabber/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='DigikeyPart',
+    version='0.1dev',
+    packages=['digikeyPartGrabber',],
+    license=' GNU GENERAL PUBLIC LICENSE -3.0',
+    long_description=open('README.md').read(),
+)
